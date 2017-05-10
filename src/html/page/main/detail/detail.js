7ae436fafbdd36b43b40acbb2b0a0a6475925f19
Law-God <767030396@qq.com>
2017-05-10 20:34:39 +0800

    'edit'

--- src/html/page/main/detail/detail.js
+++ src/html/page/main/detail/detail.js
@@ -1,13 +1,23 @@
+import mainLess from "../main.less";
 import detailLess from "./detail.less";
 
 import React,{Component} from "react";
 import { Link } from 'react-router';
 
+var gyzbImg = require("./images/gyzb.jpg");
+var jllyImg = require("./images/jlly.jpg");
+var wjctImg = require("./images/wjct.jpg");
+
+
 export default class Role extends Component{
 	render(){
 		return(
-			<main role="main" className="main">
-				<div>轮播图</div>
+			<main role="main" className={mainLess['main'] + " " + detailLess['main']}>
+				<div>
+					<div className={detailLess['picture-play']}>
+						<img src={gyzbImg} />
+					</div>
+				</div>
 				<div>导航</div>
 				<div>基本信息</div>
 				<div>剧集介绍</div>
